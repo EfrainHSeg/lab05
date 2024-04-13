744b8ebf20801d12225514d8186821ac2920a997
Efrain <Efrain@LAPTOP-PUU85PCD>
2024-04-13 16:29:30 -0500

    Agregar archivos de proyecto.

---
+++ lab05/Empleado.cs
@@ -0,0 +1,47 @@
+﻿namespace lab05
+{
+    public class Empleado
+    {
+        public int idEmpleado { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public string Tratamiento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaContratacion { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Region { get; set; }
+        public string CodPostal { get; set; }
+        public string Pais { get; set; }
+        public string TelDomicilio { get; set; }
+        public string Extension { get; set; }
+        public string Notas { get; set; }
+        public int Jefe { get; set; }
+        public decimal SueldoBasico { get; set; }
+
+        public Empleado(int idEmpleado, string apellidos, string nombre, string cargo, string tratamiento,
+                         DateTime fechaNacimiento, DateTime fechaContratacion, string direccion, string ciudad,
+                         string region, string codPostal, string pais, string telDomicilio, string extension,
+                         string notas, int jefe, decimal sueldoBasico)
+        {
+            idEmpleado = idEmpleado;
+            Apellidos = apellidos;
+            Nombre = nombre;
+            Cargo = cargo;
+            Tratamiento = tratamiento;
+            FechaNacimiento = fechaNacimiento;
+            FechaContratacion = fechaContratacion;
+            Direccion = direccion;
+            Ciudad = ciudad;
+            Region = region;
+            CodPostal = codPostal;
+            Pais = pais;
+            TelDomicilio = telDomicilio;
+            Extension = extension;
+            Notas = notas;
+            Jefe = jefe;
+            SueldoBasico = sueldoBasico;
+        }
+    }
+}
---
+++ lab05/Menu.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Windows;
+
+namespace lab05
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu  
+    {
+        //string connectionString = "Data Source= LAPTOP-PUU85PCD\\SQLEXPRESS;Initial Catalog=NeptunoDB; User ID=USER02; Password=12345;";
+        string connectionString = "Data Source= LAPTOP-PUU85PCD\\SQLEXPRESS;Initial Catalog=NeptunoDB; User ID=USER02; Password=12345;";
+        public List<Empleado> ListaEmpleado { get; set; }
+
+
+        public Menu()
+        {
+            InitializeComponent();
+            ListaEmpleado = new List<Empleado>();
+            dataEmpleados.ItemsSource = ListaEmpleado;
+        }
+
+        private void Button_Insertar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Listar(object sender, RoutedEventArgs e)
+        {
+            List<Empleado> empleados = new List<Empleado>();
+          
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("Listar_Empleado",  connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int idEmpleado = reader.GetInt32(reader.GetOrdinal("idEmpleado"));
+                            string apellidos = reader.GetString(reader.GetOrdinal("Apellidos"));
+                            string nombre = reader.GetString(reader.GetOrdinal("Nombre"));
+                            string cargo = reader.GetString(reader.GetOrdinal("Cargo"));
+                            string tratamiento = reader.GetString(reader.GetOrdinal("Tratamiento"));
+                            DateTime fechaNacimiento = reader.GetDateTime(reader.GetOrdinal("FechaNacimiento"));
+                            DateTime fechaContratacion = reader.GetDateTime(reader.GetOrdinal("FechaContratacion"));
+                            string direccion = reader.GetString(reader.GetOrdinal("Direccion"));
+                            string ciudad = reader.GetString(reader.GetOrdinal("Ciudad"));
+                            string region = reader.GetString(reader.GetOrdinal("Region"));
+                            string codPostal = reader.GetString(reader.GetOrdinal("CodPostal"));
+                            string pais = reader.GetString(reader.GetOrdinal("Pais"));
+                            string telDomicilio = reader.GetString(reader.GetOrdinal("TelDomicilio"));
+                            string extension = reader.GetString(reader.GetOrdinal("Extension"));
+                            string notas = reader.GetString(reader.GetOrdinal("Notas"));
+                            int jefe = reader.GetInt32(reader.GetOrdinal("Jefe"));
+                            decimal sueldoBasico = reader.GetDecimal(reader.GetOrdinal("SueldoBasico"));
+
+                            empleados.Add(new Empleado(idEmpleado, apellidos, nombre, cargo, tratamiento,
+                                fechaNacimiento, fechaContratacion, direccion, ciudad,
+                                region, codPostal, pais, telDomicilio, extension,
+                                notas, jefe, sueldoBasico));
+                        }
+                    }
+                }
+
+                dataEmpleados.ItemsSource = empleados; 
+                dataEmpleados.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
+
+
+
